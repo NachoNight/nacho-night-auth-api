0b12ddff022da2b2ab42b421835bec06d355ad87
denis-onder <denis_onder@protonmail.com>
2019-09-28 00:03:28 +0200

    Setting up the testing suite and fixed the logger

--- app/middleware/logger.js
+++ app/middleware/logger.js
@@ -11,9 +11,18 @@ const morgan = require("morgan");
 const fs = require("fs");
 const path = require("path");
 
-module.exports = app =>
+module.exports = app => {
+  const folderPath = path.resolve(__dirname, "../logs");
+  // Create the logs folder if it does not exist
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath);
+  }
+  const stream = fs.createWriteStream(path.join(folderPath, "access.log"), {
+    flags: "a"
+  });
   app.use(
     morgan("combined", {
-      stream: fs.createWriteStream(path.resolve(__dirname, "../logs"))
+      stream
     })
   );
+};
---
+++ app/test/test.test.js
@@ -0,0 +1,5 @@
+const { expect } = require("chai");
+
+describe("Testing Suite", () => {
+  it("should return 4", () => expect(2 + 2).to.eq(4));
+});
